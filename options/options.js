3e232c9074c49fac2fa68041477f7b28ffa8b1a4
Daniel Palacios <daniel80380@gmail.com>
2023-03-21 10:43:15 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,7 +1,8 @@
 export const globalOptions = {
-  name: "t10371-dev-70026",
-  url: "https://t10371-dev-70026.botics.co",
-  api: "https://t10371-dev-70026.botics.co/api/v1"
+  "name": "t10371-dev-70026",
+  "url": "https://t10371-dev-70026.botics.co",
+  "api": "https://t10371-dev-70026.botics.co/api/v1",
+  "initialRoute": "Untitled2"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
